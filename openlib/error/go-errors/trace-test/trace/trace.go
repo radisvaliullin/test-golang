752f8bc6c0f12589c87406fb121b8dab62ddf962
Radis Valiullin <radisvaliullin@gmail.com>
2019-04-03 11:59:59 +0300

    openlib: error: go-errors: stack trace test 2

--- openlib/error/go-errors/trace-test/trace/trace.go
+++ openlib/error/go-errors/trace-test/trace/trace.go
@@ -8,6 +8,14 @@ import (
 
 func Trace() {
 	e := errors.New("test errr")
+	TracePrint(e)
+	// est := e.ErrorStack()
+	// log.Printf("error stack - %v", est)
+}
+
+func TracePrint(e *errors.Error) {
+	// e = errors.New("test errr 2")
+	log.Print("trace print")
 	est := e.ErrorStack()
 	log.Printf("error stack - %v", est)
 }
