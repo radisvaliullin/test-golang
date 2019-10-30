54de5f73d3ac2a0b4023423af835aea172a9842f
Radis Valiullin <radisvaliullin@gmail.com>
2019-10-30 15:57:36 -0700

    spec: types: slice: len of nil and empty slices

--- spec/types/slice/zelo-len-slice/main.go
+++ spec/types/slice/zelo-len-slice/main.go
@@ -9,6 +9,7 @@ func main() {
 	sl2 := []byte{1}
 	fmt.Printf("%v : %v : %v\n", sl0, sl1, sl2)
 
+	// is nil
 	if sl0 == nil {
 		fmt.Println("sl0 is nil")
 	}
@@ -17,5 +18,16 @@ func main() {
 	}
 	if sl2 == nil {
 		fmt.Println("sl2 is nil")
+	}
+
+	// len
+	if len(sl0) == 0 {
+		fmt.Println("sl0 len is 0")
+	}
+	if len(sl1) == 0 {
+		fmt.Println("sl1 len is 0")
+	}
+	if len(sl2) == 0 {
+		fmt.Println("sl2 len in 0")
 	}
 }
