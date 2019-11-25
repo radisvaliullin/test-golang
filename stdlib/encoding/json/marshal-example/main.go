3a68ed0b91f160cc1dce67c2080fb597f2de7ef6
Radis Valiullin <radisvaliullin@gmail.com>
2019-11-24 21:40:50 -0800

    stdlib: encoding: json: marshal inheritance

--- stdlib/encoding/json/marshal-example/main.go
+++ stdlib/encoding/json/marshal-example/main.go
@@ -5,21 +5,30 @@ import (
 	"log"
 )
 
+type exampleBase struct {
+	One    int    `json:"one"`
+	OneStr string `json:"one_str"`
+}
+
+//
+type example struct {
+	exampleBase
+	IntNull  *int `json:"int_null"`
+	IntNull2 *int `json:"int_null_2"`
+	IntNull3 *int `json:"int_null_3,omitempty"`
+}
+
 func main() {
 
 	i := 66
 
-	example := struct {
-		IntNull  *int `json:"int_null"`
-		IntNull2 *int `json:"int_null_2"`
-		IntNull3 *int `json:"int_null_3,omitempty"`
-	}{
+	e := example{
 		IntNull:  &i,
 		IntNull2: nil,
 		IntNull3: nil,
 	}
 
-	bt, err := json.Marshal(&example)
+	bt, err := json.Marshal(&e)
 	if err != nil {
 		log.Fatal("marshal err: ", err)
 	}
