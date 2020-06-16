180168c2edd503f6c58ed21f5d0cc44da45d3d53
radis <radisvaliullin@gmail.com>
2020-06-16 11:56:38 -0700

    stdlib: time: format: unix to time and back

---
+++ stdlib/time/format/unix-to-time/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+func main() {
+
+	t := time.Unix(1591888922635544035/int64(time.Second), 1591888922635544035%int64(time.Second))
+	fmt.Println(t)
+
+	t2, err := time.Parse("2006-01-02 15:04:05 -0700 UTC", "2020-06-11 15:26:11 +0000 UTC")
+	if err != nil {
+		log.Fatalf("time parse error %v\n", err)
+	}
+	fmt.Println(t2)
+	fmt.Println(t2.Unix())
+	fmt.Println("1591889177822920785")
+}
