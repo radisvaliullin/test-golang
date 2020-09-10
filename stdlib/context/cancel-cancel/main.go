a3fe281aaa77e6ce3c77f3d01892563242bacb1a
radis <radisvaliullin@gmail.com>
2020-09-09 19:39:43 -0700

    stdlib: context: cancel cancel

--- stdlib/context/cancel-cancel/main.go
+++ stdlib/context/cancel-cancel/main.go
@@ -7,14 +7,23 @@ func main() {
 	ctx := context.Background()
 
 	ctx2, cancel := context.WithCancel(ctx)
+	ctx3, cancel3 := context.WithCancel(ctx2)
 
 	cancel()
 	cancel()
+	cancel3()
+	cancel3()
 
 	select {
 	case <-ctx2.Done():
 		println("ctx2 done")
 	default:
 		println("ctx2 do NOT done")
+	}
+	select {
+	case <-ctx3.Done():
+		println("ctx3 done")
+	default:
+		println("ctx3 do NOT done")
 	}
 }
