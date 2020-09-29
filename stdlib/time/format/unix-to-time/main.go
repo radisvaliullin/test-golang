df4b225a811d076d9b5ccba9f6543b2d64608d82
radis <radisvaliullin@gmail.com>
2020-09-29 14:36:55 -0700

    stdlib: time: format: unit to time

--- stdlib/time/format/unix-to-time/main.go
+++ stdlib/time/format/unix-to-time/main.go
@@ -9,7 +9,9 @@ import (
 func main() {
 
 	t := time.Unix(1591888922635544035/int64(time.Second), 1591888922635544035%int64(time.Second))
+	tt := time.Unix(0, 1591888922635544035)
 	fmt.Println(t)
+	fmt.Println(tt)
 
 	t2, err := time.Parse("2006-01-02 15:04:05 -0700 UTC", "2020-06-11 15:26:11 +0000 UTC")
 	if err != nil {
