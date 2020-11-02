b4724457fdd5b50537d6967fd408c3e3ae82c5a3
radis <radisvaliullin@gmail.com>
2020-11-02 00:14:27 -0800

    stdlib: bytes: buffer, base

--- stdlib/bytes/bufffer/base/main.go
+++ stdlib/bytes/bufffer/base/main.go
@@ -7,10 +7,10 @@ import (
 
 func main() {
 
-	buf := bytes.NewBuffer(make([]byte, 0, 32))
+	buf := bytes.NewBuffer(make([]byte, 0, 8))
 	fmt.Println(buf.Len(), buf.Cap())
 
-	buf.Write([]byte{32})
+	buf.Write([]byte{32, 1, 2, 3, 4, 5, 6, 7, 8})
 	fmt.Println(buf.Len(), buf.Cap())
 
 	fmt.Println(buf.Bytes())
