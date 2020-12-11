3f028e8e33177d3326fcc7aa2694340d86274407
radis <radisvaliullin@gmail.com>
2020-12-10 16:28:13 -0800

    test: muxtex cost: single thread cost

--- test/mutex-cost/cost-single-thread/main.go
+++ test/mutex-cost/cost-single-thread/main.go
@@ -11,13 +11,15 @@ func main() {
 	var loopSize = 1_000_000_000
 
 	var inc int
+	var now time.Time
+	var since time.Duration
 
 	// inc without mutex
-	now := time.Now()
+	now = time.Now()
 	for i := 0; i < loopSize; i++ {
 		inc++
 	}
-	since := time.Since(now)
+	since = time.Since(now)
 	fmt.Println("loop time:", since)
 
 	// inc with mutex
@@ -31,4 +33,38 @@ func main() {
 	}
 	since = time.Since(now)
 	fmt.Println("loop time with mutex cost:", since)
+
+	// fibo
+	fb := fibo{}
+	now = time.Now()
+	for i := 0; i < loopSize; i++ {
+		_ = fb.next()
+	}
+	since = time.Since(now)
+	fmt.Println("next fibo loop time:", since)
+
+	// fibo mutex
+	fb = fibo{}
+	now = time.Now()
+	for i := 0; i < loopSize; i++ {
+		mx.Lock()
+		_ = fb.next()
+		mx.Unlock()
+	}
+	since = time.Since(now)
+	fmt.Println("next fibo loop time:", since)
+}
+
+type fibo struct {
+	prev int
+	curr int
+}
+
+func (f *fibo) next() int {
+	if f.prev == 0 && f.curr == 0 {
+		f.curr = 1
+		return f.prev
+	}
+	f.prev, f.curr = f.curr, f.prev+f.curr
+	return f.prev
 }
